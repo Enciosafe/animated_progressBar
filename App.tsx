832e04cdf5a2c1f3b2ee75634125cbd53bfeabbf
Stas <finneganlucky@gmail.com>
2022-11-14 11:52:15 +0100

    main

--- App.tsx
+++ App.tsx
@@ -1,12 +1,84 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { FC, useEffect, useRef, useState } from 'react';
+import {Animated, StyleSheet, Text, View } from 'react-native';
+
+interface ProgressProps {
+    step: number,
+    steps: number,
+    height: number
+
+}
+
+const Progress:FC<ProgressProps> = ({step, steps, height}) => {
+    const [width, setWidth] = useState(0);
+    const animatedValue = useRef(new Animated.Value(-1000)).current
+    const reactive = useRef(new Animated.Value(-1000)).current
+
+    useEffect(() => {
+        Animated.timing(animatedValue, {
+            toValue: reactive,
+            duration: 300,
+            useNativeDriver: true
+        }).start()
+    }, []);
+
+    useEffect(() => {
+        reactive.setValue(-width + (width * step) / steps)
+    }, [step, width]);
+
+
+
+    return (
+        <>
+            <Text style={{fontFamily: 'Menlo', fontSize: 12, fontWeight: '900', marginBottom: 4}}>
+                {step}/{steps}
+            </Text>
+            <View style={{
+                height,
+                backgroundColor: 'rgba(0,0,0,0.1)',
+                borderRadius: height,
+                overflow: 'hidden'
+            }}>
+                <Animated.View
+                    onLayout={e => {
+                        const newWidth = e.nativeEvent.layout.width;
+                        setWidth(newWidth)
+                    }}
+                    style={{
+                    height,
+                    width: '100%',
+                    borderRadius: height,
+                    backgroundColor: 'rgba(0,0,0,0.5)',
+                    position: 'absolute',
+                    left: 0,
+                    top: 0,
+                    transform: [{
+                        translateX: animatedValue
+                    }]
+
+                }}/>
+            </View></>
+
+    )
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex((index + 1) % (10 + 1))
+        }, 300)
+        return () => {
+            clearInterval(interval)
+        }
+    }, [index]);
+
+    return (
+      <View style={styles.container}>
+        <StatusBar hidden />
+        <Progress step={index} steps={10} height={20}/>
+      </View>
   );
 }
 
@@ -14,7 +86,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    padding: 20
   },
 });
